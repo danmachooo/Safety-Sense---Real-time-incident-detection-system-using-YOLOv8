3b65dcb842ddeff0877e6911ed64239c44ce4222
danmachooo <johnpauldanmachi@gmail.com>
2025-01-27 17:22:07 +0800

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config()
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json({urlencoded:true}));
+
+
+//Start backend
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

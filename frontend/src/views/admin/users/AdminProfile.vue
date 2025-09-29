2de651d49fb44c4a99c08f159af73a1a2600cb59
danmachooo <johnpauldanmachi@gmail.com>
2025-09-29 15:58:02 +0800

    Fixed a bug in AdminProfile.vuev2

--- frontend/src/views/admin/users/AdminProfile.vue
+++ frontend/src/views/admin/users/AdminProfile.vue
@@ -49,9 +49,12 @@ const editForm = ref({
 
 const fetchProfile = async () => {
   try {
-    if (!authUser?.id) return;
-
+    if (!authUser?.id) {
+      console.error("Auth user is not provided");
+      return;
+    }
     const response = await api.get(`manage-user/get/${authUser.id}`);
+    console.log("Auth User: ", authUser);
     user.value = {
       ...response.data.data,
       createdAt: new Date(response.data.data.createdAt).toLocaleDateString(

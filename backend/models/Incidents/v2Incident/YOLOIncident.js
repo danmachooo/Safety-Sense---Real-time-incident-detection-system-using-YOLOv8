8bc1a4271d271070a715905a3dd69b4bfaac5674
Thiena-Damasco <thiena8@gmail.com>
2025-10-31 16:37:44 +0800

    Hello world

--- backend/models/Incidents/v2Incident/YOLOIncident.js
+++ backend/models/Incidents/v2Incident/YOLOIncident.js
@@ -24,7 +24,7 @@ const YOLOIncident = sequelize.define(
     },
     detectionFrameUrl: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     detectedObjects: {
       type: DataTypes.JSON,
